dd9ad4fc2070abd01b26d33acfb363a2323caf27
Mekhola Doha <mekholadoha@Mekholas-MacBook-Air.local>
2023-12-18 00:29:52 +0100

    test 2 fail, the previous commit didn't show it failed please disregard

--- part_1/src/mastermind.py
+++ part_1/src/mastermind.py
@@ -9,19 +9,9 @@ class CodeMaker:
     def generate_code(self):
         return ''.join(random.choice(self.initials) for _ in range(4))
 
-    def provide_feedback(self, guess):
-        correct_positions = sum(g == c for g, c in zip(guess, self.code))
-        code_copy = list(self.code)
-
-        for i, color in enumerate(guess):
-            if color == self.code[i]:
-                code_copy[i] = None
-        correct_colors_wrong_positions = 0
-        for i, color in enumerate(guess):
-            if color in code_copy:
-                correct_colors_wrong_positions += 1
-                code_copy[code_copy.index(color)] = None
-        return correct_positions, correct_colors_wrong_positions
+    def provide_feedback(self):
+        pass
+        
 
 
 class CodeBreaker:
