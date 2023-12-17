b2a23ded4d6f21a4b1d35dd065eaefe9f7c643b2
Mekhola Doha <mekholadoha@Mekholas-MacBook-Air.local>
2023-12-17 17:39:08 +0100

    First Test fail edited AGAIN

--- part_1/src/mastermind.py
+++ part_1/src/mastermind.py
@@ -1,10 +1,6 @@
-import random
-
-
 class CodeMaker:
     def __init__(self):
-        self.valid_symbols = ['W', 'B', 'Y', 'G', 'R', 'X']
-        self.secret_code = self.generate_code()
+        pass
 
-    def code_generation(self):
-        return [random.choice(self.valid_symbols) for _ in range(4)]
+    def generate_code(self):
+        pass
