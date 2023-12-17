f97b0a1430ae732df4dfdde9522245d1d5d934af
Mekhola Doha <mekholadoha@Mekholas-MacBook-Air.local>
2023-12-17 17:43:25 +0100

    First Test pass

--- part_1/src/mastermind.py
+++ part_1/src/mastermind.py
@@ -1,6 +1,10 @@
+import random
+
+
 class CodeMaker:
-    def __init__(self):
-        pass
+    def __init__(self) -> None:
+        self.initials = "WRGYBX"
+        self.code = self.generate_code()
 
     def generate_code(self):
-        pass
+        return ''.join(random.choice(self.initials) for _ in range(4))
