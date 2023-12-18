1ebd9b7c9e9041e6c606e538abca6c1f9a07eb2d
Mekhola Doha <mekholadoha@Mekholas-MacBook-Air.local>
2023-12-18 01:30:47 +0100

    test 3 fail I forgot to commit the fail here

--- part_1/src/mastermind.py
+++ part_1/src/mastermind.py
@@ -10,22 +10,7 @@ class CodeMaker:
         return ''.join(random.choice(self.initials) for _ in range(4))
 
     def provide_feedback(self, guess):
-        correct_position = sum(g == c for g, c in zip(guess, self.code))
-        correct_colors_wrong_positions = 0
-        code_copy = list(self.code)
-        guess_copy = list(guess)
-
-        for i in range(len(guess)):
-            if guess[i] == self.code[i]:
-                code_copy[i] = None
-                guess_copy[i] = None
-
-        for color in guess_copy:
-            if color and color in code_copy:
-                correct_colors_wrong_positions += 1
-                code_copy[code_copy.index(color)] = None
-
-        return correct_position, correct_colors_wrong_positions
+        pass
 
 
 class CodeBreaker:
